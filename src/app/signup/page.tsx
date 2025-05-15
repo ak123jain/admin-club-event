fa0dd19f2525b23d390593edf3bc3c6a5b197b9c
AkASH jain <aj9882854@gmail.com>
2025-05-16 01:07:06 +0530

    ijj

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -59,7 +59,7 @@ const Page: React.FC = () => {
         headers: { 'Content-Type': 'multipart/form-data' },
       })
 
-      console.log(response);
+      console.log( "💕💕💕💕💕💕" ,  response);
       
 
       if (response.data.message === 'Admin created successfully') {
